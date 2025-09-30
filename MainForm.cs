d5bf0734fe0fe55458c6dd9bedadaab6d8a76ed8
seung il baek <seung6945@gmail.com>
2025-10-01 07:55:24 +0900

    기본 세팅 및 SunnyUI 설치 완료

--- MainForm.cs
+++ MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace _3D_VisionSource
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
